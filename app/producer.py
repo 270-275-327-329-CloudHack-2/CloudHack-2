93793fa1973cf322ca7f3b42ee2a47d07524923c
Joy Jefferson <PES2UG19CS270@pesu.pes.edu>
2022-03-20 10:13:22 +0530

    added stuff

--- app/producer.py
+++ app/producer.py
@@ -1,1 +1,44 @@
 # Code for Producer
+from flask import Flask, jsonify, request
+import pika
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/new_ride', methods=['POST'])
+def new_ride(payload):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body= payload,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    connection.close()
+    return " [x] Sent: %s" % payload
+
+@app.route('/new_ride_matching_consumer', methods=['POST'])
+def new_ride_matching_consumer(payload):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body= payload,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    connection.close()
+    return " [x] Sent: %s" % payload
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
